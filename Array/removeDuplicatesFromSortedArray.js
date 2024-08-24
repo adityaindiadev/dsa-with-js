8125b326d171689ec1d10b71c0a813c1583b9ee0
unknown <aditya.india.dev@gmail.com>
2024-08-24 19:48:32 +0530

    removeDuplicatesFromSortedArray

---
+++ Array/removeDuplicatesFromSortedArray.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let left = 0
+    let right = 1
+    let check = 1
+
+    while (right < nums.length && left <= right) {
+        if (nums[right] == nums[right - 1]) {
+
+            if (check < 2) {
+                check = 2
+                left++
+                nums[left] = nums[right]
+            }
+            right++
+        }
+        else {
+            check = 1
+            left++
+            nums[left] = nums[right]
+            right++
+        }
+    }
+
+    // console.log(nums);
+    return left + 1
+
+};
