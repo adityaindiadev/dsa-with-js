6e4e595312329a21190f40020b0a2dc5de496162
unknown <aditya.india.dev@gmail.com>
2024-09-04 22:48:28 +0530

    77. Combinations

---
+++ Backtracking/combinations.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+function performPermutation(perm, kIdx, nIdx, n, k, finalArray) {
+    if (kIdx > k) {
+        finalArray.push(perm)
+        return
+    }
+    let i = nIdx
+    while (i <= n) {
+        performPermutation([...perm, i], kIdx + 1, i + 1, n, k, finalArray)
+        i++
+    }
+}
+var combine = function (n, k) {
+    const finalArray = []
+    performPermutation([], 1, 1, n, k, finalArray)
+    return finalArray
+};
