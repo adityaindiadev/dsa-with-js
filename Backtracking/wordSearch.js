842a99acd5c88bf173cd42fdabd297df83c2c21f
unknown <aditya.india.dev@gmail.com>
2024-09-13 06:55:32 +0530

    SOLVE PENDING

--- Backtracking/wordSearch.js
+++ Backtracking/wordSearch.js
@@ -6,12 +6,12 @@
  * @return {boolean}
  */
 var exist = function (board, word) {
-    return initiateWordSearch(board, word, 0, 0, 0)
+    return initiateWordSearch(board, word, 0, 0, 0, {})
 };
 function initiateWordSearch(board, word, row, column, wordCharIndex, hashMap) {
-
+    console.log('initiateWordSearch', { board, word, row, column, wordCharIndex, hashMap })
     if (wordCharIndex >= word.length) {
-        console.log('wordCharIndex >= word.length', { board, word, row, column, wordCharIndex });
+        console.log('wordCharIndex >= word.length', { board, word, row, column, wordCharIndex, hashMap });
         return true
     }
     if (row >= board.length)
@@ -25,19 +25,24 @@ function initiateWordSearch(board, word, row, column, wordCharIndex, hashMap) {
     if (board[row, column] == undefined) {
         return false
     }
+    if (hashMap[`${row}${column}`]) {
+        console.log("hashMap False", { board, word, row, column, wordCharIndex, hashMap });
+
+        return false
+    }
     if (board[row][column] != word[wordCharIndex]) {
         return false
     }
-    if (initiateWordSearch(board, word, row, column + 1, wordCharIndex + 1) || initiateWordSearch(board, word, row + 1, column, wordCharIndex + 1) || initiateWordSearch(board, word, row, column - 1, wordCharIndex + 1) || initiateWordSearch(board, word, row - 1, column, wordCharIndex + 1)) {
-        console.log('if', { board, word, row, column, wordCharIndex });
+    if (initiateWordSearch(board, word, row, column + 1, wordCharIndex + 1, { ...hashMap, [`${row}${column}`]: 1 }) || initiateWordSearch(board, word, row + 1, column, wordCharIndex + 1, { ...hashMap, [`${row}${column}`]: 1 }) || initiateWordSearch(board, word, row, column - 1, wordCharIndex + 1, { ...hashMap, [`${row}${column}`]: 1 }) || initiateWordSearch(board, word, row - 1, column, wordCharIndex + 1, { ...hashMap, [`${row}${column}`]: 1 })) {
+        console.log('if', { board, word, row, column, wordCharIndex, hashMap });
         return true
     }
     else {
-        return (initiateWordSearch(board, word, row, column + 1, 0) || initiateWordSearch(board, word, row + 1, column, 0) || initiateWordSearch(board, word, row, column - 1, 0) || initiateWordSearch(board, word, row - 1, column, 0))
+        return (initiateWordSearch(board, word, row, column + 1, 0, { ...hashMap, [`${row}${column}`]: 1 }) || initiateWordSearch(board, word, row + 1, column, 0, { ...hashMap, [`${row}${column}`]: 1 }) || initiateWordSearch(board, word, row, column - 1, 0, { ...hashMap, [`${row}${column}`]: 1 }) || initiateWordSearch(board, word, row - 1, column, 0, { ...hashMap, [`${row}${column}`]: 1 }))
     }
 
 
 }
 const board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
-const word = "ABCB"
+const word = "ABCE"
 console.log(exist(board, word));
